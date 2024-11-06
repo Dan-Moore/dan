cc239c92a0f02001392df1a7ae1e1429814f56d8
Dan Moore <grep.daniel@gmail.com>
2024-11-05 23:02:03 -0500

    ruff draft

--- lib/utils.ts
+++ lib/utils.ts
@@ -15,33 +15,26 @@ export const CONTENT_DIR = path.join(process.cwd(), "content");
 export const CONTENT_PAGES_DIR = path.join(CONTENT_DIR, "pages");
 export const CONTENT_POSTS_DIR = path.join(CONTENT_DIR, "posts");
 export const CONTENT_PROJECTS_DIR = path.join(CONTENT_DIR, "projects");
-export const CONTENT_DOCS_DIR = path.join(CONTENT_DIR, "docs");
 
+// Paths
+export const PAGES_PATHS = fs.readdirSync(CONTENT_PAGES_DIR, {recursive: true})
+  .filter((path) => /\.mdx?$/.test(path.toString()));
 
-export const CONTENT_PAGE_PATHS = fs.readdirSync(CONTENT_PAGES_DIR)
- .filter((path) => /\.mdx?$/.test(path));
-export const CONTENT_PROJECT_PATHS = fs.readdirSync(CONTENT_PAGES_DIR)
- .filter((path) => /\.mdx?$/.test(path));
+export const POSTS_PATHS = fs.readdirSync(CONTENT_POSTS_DIR, {recursive: true})
+  .filter((path) => /\.mdx?$/.test(path.toString()));
 
+export const PROJECTS_PATHS = fs.readdirSync(CONTENT_PROJECTS_DIR, {recursive: true})
+  .filter((path) => /\.mdx?$/.test(path.toString()));
 
-export function getContents(content_dir: string[]) {
-  const mdx_file = content_dir.map((filePath: string) => {
-    const source = fs.readFileSync(path.join(CONTENT_PAGES_DIR, filePath));
-    const { content, data } = matter(source);
-
-    return {
-      content,
-      data,
-      filePath,
-    };
-  });
-
-  return { props: { mdx_file } };
-}
-
+// Markdown
 export async function getMarkdown(dir: string, slug: string) {
+  if(!slug.endsWith(".mdx")){
+    slug + ".mdx"
+  }
+
   const markdownFile = fs.readFileSync(
-    path.join(dir, slug + ".mdx"),
+    path.join(dir, slug),
+    //path.join(dir, slug),
     "utf-8"
   );
   const { data: frontMatter, content } = matter(markdownFile);
@@ -51,6 +44,7 @@ export async function getMarkdown(dir: string, slug: string) {
     content,
   };
 }
+
 
 export async function getPages({ slug }: { slug: string }) {
   return getMarkdown(CONTENT_PAGES_DIR, slug);
@@ -62,8 +56,4 @@ export async function getPosts({ slug }: { slug: string }) {
 
 export async function getProjects({ slug }: { slug: string }) {
   return getMarkdown(CONTENT_PROJECTS_DIR, slug);
-}
-
-export async function getDocs({ slug }: { slug: string }) {
-  return getMarkdown(CONTENT_DOCS_DIR, slug);
 }
